491221ef4df806b7272bf8c52883f5a8ba4f3de4
FrederikDegn <qwn116@alumni.ku.dk>
2022-03-02 10:01:52 +0100

    exercise 6

--- 1_theory_tools/exercises/ex_ante/Exercise_6.py
+++ 1_theory_tools/exercises/ex_ante/Exercise_6.py
@@ -27,11 +27,10 @@ def solve_consumption_deaton(par):
         
             if t<par.T-1:
                 for s in range(par.num_shocks):
-                    # fill in
                     
-                    
-                    
-                    
+                    weight = par.eps_w[s]
+                    eps = par.eps[s]
+                    EV_next += weight * np.interp(w_c + eps,sol.grid_W[:,t+1],sol.V[:,t+1])
                     
                     
             V_guess = util(c,par)+par.beta*EV_next
